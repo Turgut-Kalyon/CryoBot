da2e73ca530c452e48e50871033d3846bba66dea
Turgut Kalyon <218080435+Turgut-Kalyon@users.noreply.github.com>
2025-08-14 20:17:56 +0200

    style: enhance Jenkins pipeline to pass virtual environment directory to runScript function

--- tests/jenkins/jenkinsfile.groovy
+++ tests/jenkins/jenkinsfile.groovy
@@ -30,7 +30,7 @@ pipeline{
                 script{
                     sh '''
                     python3 -m venv ${VENV_DIR}
-                    source ${VENV_DIR}/bin/activate
+                    . ${VENV_DIR}/bin/activate
                     pip install --upgrade pip
                     pip install -r requirements.txt
                     '''
