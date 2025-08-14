a04b638b7ba334ed3b920df471d151691b5535d1
Turgut Kalyon <218080435+Turgut-Kalyon@users.noreply.github.com>
2025-08-14 22:05:33 +0200

    test: update load assertion to verify saved value correctly

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -72,9 +72,9 @@ class TestUnitStorage:
         print(file)
         storage.set("test_save_load_user", 700)
         print(loaded_storage.get("test_save_load_user"))
-        loaded_storage.load()
+
         print(loaded_storage.get("test_save_load_user"))
-        assert loaded_storage.get("test_save_load_user") == 700, "Save and Load failed"
+        assert loaded_storage.load()["test_save_load_key"].get("test_save_load_user", None) == 700, "Save and Load failed"
 
 
 
