a8582ed68f47149452188e1af0aa081da33258b3
Turgut Kalyon <218080435+Turgut-Kalyon@users.noreply.github.com>
2025-08-15 21:39:42 +0200

    fix: update test script paths in Jenkinsfile for improved organization

--- tests/jenkins/jenkinsfile.groovy
+++ tests/jenkins/jenkinsfile.groovy
@@ -4,9 +4,9 @@ pipeline{
     agent any
     environment {
         BUILD_DIR = "${WORKSPACE}/tests"
-        Script_for_UnitTestStorage = "pytest -v ${BUILD_DIR}/test_unittest_Storage.py::TestUnitStorage"
-        Script_for_UnitTestCoinTransfer = "pytest -v ${BUILD_DIR}/test_unittest_CoinTransfer.py::TestUnitCoinTransfer"
-        Script_for_IntigrationTestCracc = "pytest -v ${BUILD_DIR}/test_integration.py::TestIntegrationCracc"
+        Script_for_UnitTestStorage = "pytest -v ${BUILD_DIR}/unittests/test_unittest_Storage.py::TestUnitStorage"
+        Script_for_UnitTestCoinTransfer = "pytest -v ${BUILD_DIR}/unittests/test_unittest_CoinTransfer.py::TestUnitCoinTransfer"
+        Script_for_IntigrationTestCracc = "pytest -v ${BUILD_DIR}/Integrationtests/test_integration.py::TestIntegrationCracc"
         RESULT_DIR = "${WORKSPACE}/results"
     }
 
